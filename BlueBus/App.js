3f34eb4f36bbb7ff47cfcda6190e69101a779cf6
Arthur <arthur.chang@live.com>
2018-05-03 16:59:37 -0400

    Now have a very simple app that counts up every second since the app was started

--- BlueBus/App.js
+++ BlueBus/App.js
@@ -1,29 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { updateClock } from './haverford-timer';
-import { BackgroundTimer } from 'react-native-background-timer';
 
 export default class App extends React.Component {
 	constructor() {
       super()
       this.state = {
-         myText: "This is a placeholder text"
-	  }
-	  this.i = 0;
-	  if (this.timer>0) return;   // Already started
-	  this.timer = BackgroundTimer.setInterval(() => {
-		  // this will be executed every 200 ms
-		  // even when app is the the background
-		  this.setState({myText: this.i});
-		  this.i++;
-	  }, 1000);
+         myText: "This is a placeholder text",
+		 i: 0,
+		 isShowingText: true
+	  };
+	  setInterval(() => {
+      this.setState(previousState => {
+        return { i: previousState.i + 1 };
+      });
+    }, 1000);
 
    }
     render() {
 		return (
 		  <View style={styles.container}>
 			<Text>
-				   {this.state.myText}
+				   {this.state.i}
 			</Text>
 		  </View>
     );
